0f588cd4b1a3d1eb04be94cfa26fe35349e22795
PapejaJr <papejajunior@gmail.com>
2020-01-12 15:37:23 -0300

    Updates

---
+++ precache-manifest.eb5a5cfbfd0e5a63fe2ef82d9ed56806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0a6fbfaf9a8179b7a481acdd6f7b2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "034a58c4c6f0f702300a",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5e9d8c3eef66892d844d",
+    "url": "/static/js/2.998d0a95.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.998d0a95.chunk.js.LICENSE"
+  },
+  {
+    "revision": "034a58c4c6f0f702300a",
+    "url": "/static/js/main.1050fc3a.chunk.js"
+  },
+  {
+    "revision": "50158a8623f208d3c6df",
+    "url": "/static/js/runtime-main.da689c77.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
